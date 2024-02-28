ab9b11c99e493f8a5d693bbd727f98cb858a6b1e
Yury <yuury.chang@gmail.com>
2024-02-28 21:19:42 +0800

    Clean up

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,20 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function main() {
-  await prisma.user.createMany({
-    data: [
-      {
-        name: 'Alice',
-        email: 'alice@gmail.com',
-      },
-      {
-        name: 'Bob',
-        email: 'bob@gmail.com',
-      },
-    ],
-  });
-}
+async function main() {}
 
 main()
   .then(async () => {
